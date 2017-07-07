ceb0601c961685c62816c53eb5c9383ce65792cd
anmolv07 <anmolvohra94@gmail.com>
2017-07-07 11:49:28 -0400

    For loop to get SNP IDs and creating FASTA files
    Added a for loop to extract all the SNP IDs for diseases and create FASTA files

--- anmol_robot2.sh
+++ anmol_robot2.sh
@@ -8,7 +8,7 @@
 #
 ##################
 
-## it is worth noting that when selecting SRA accessions, SNPs that hit do not hit RNA are not useful for RNAseq or WXS.  Those SNPs can be filtered by this method
+## it is worth noting that when selecting SRA accessions, SNPs that do not hit RNA are not useful for RNAseq or WXS.  Those SNPs can be filtered by this method
 #  for i in `cat SNP_finder/snp_list.foo`; do echo $i; esearch -query "$i" -db snp | esummary | xtract -pattern DocumentSummary -element DOCSUM | egrep -o 'NM[^,]+' | sort -u; done 
 
 ## how to get accessions for RNA and protein from SNPs
@@ -22,6 +22,15 @@
 echo ">"$1"_SEQ" >> SNP_SEQs.foo
 
 esearch -query $1 -db snp | esummary | xtract -pattern DocumentSummary -element DOCSUM | egrep -o 'SEQ[^|]+' | sort -u | awk -F"SEQ=" '{print $2}' | sed 's/\[//' | sed 's/\/\w\]//' | sed 's/\///g' >> SNP_SEQs.foo
+
+
+## For loop for extracting sequences from the SNP IDs, and compiling in fasta files
+
+for DISEASE in $(cat interesting_phenotypes);do SNP_IDs=$(echo ${DISEASE}_snps);FASTA_FILES=$(echo ${DISEASE}.fasta); for i in $(cat $SNP_IDs); do echo ">${i}_SEQ" >> $FASTA_FILES            
+> esearch -query $i -db snp | esummary | xtract -pattern DocumentSummary -element DOCSUM | egrep -o 'SEQ[^|]+' | sort -u | awk -F"SEQ=" '{print $2}' | sed 's/\[//' | sed 's/\/\w\]//' | sed 's/\///g' >> $FASTA_FILES
+> done
+> done
+
 
 ###
 
